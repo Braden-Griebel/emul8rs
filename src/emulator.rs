9cc69888271276f2cfed825a92fbfc2ae58f48e6
Braden Griebel <bgriebel@uw.edu>
2025-12-10 20:58:58 -0800

    feat(emulator): Added sprite drawing and instruction fetch Added functions for drawing a sprite (to the internal display array, not to the actual display yet), and for fetching an instruction.

--- src/emulator.rs
+++ src/emulator.rs
@@ -1,15 +1,17 @@
+// Std uses
 use std::path::Path;
 use std::sync::{Arc, Mutex, mpsc};
 use std::thread;
 use std::time::{Duration, Instant};
 
+// External uses
 use anyhow::{Context, Result, bail};
 
 // Display Constants
-const NUM_ROWS: usize = 32;
-const NUM_COLS: usize = 64;
+const DISPLAY_ROWS: usize = 32;
+const DISPLAY_COLS: usize = 64;
 const COL_STRIDE: usize = 1;
-const ROW_STRIDE: usize = 128;
+const ROW_STRIDE: usize = DISPLAY_COLS;
 
 // Emulator constants
 const MAX_STACK_SIZE: usize = 128;
@@ -17,11 +19,40 @@ const NUM_REGISTERS: usize = 16;
 const MILLIS_PER_SECOND: u64 = 1_000;
 const TIMER_HZ: u64 = 60;
 const GAME_MEMORY_START: usize = 0x200;
+const INSTRUCTION_LENGTH: usize = 2;
+
+// Sprite constants
+const SPRITE_WIDTH: usize = 8;
+
+// Font
+const FONT_START_POSITION: usize = 0x50;
+const FONT_HEIGHT: usize = 5;
+const FONT_CHAR_COUNT: usize = 16;
+const FONT: [u8; FONT_HEIGHT * FONT_CHAR_COUNT] = [
+    0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+    0x20, 0x60, 0x20, 0x20, 0x70, // 1
+    0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+    0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+    0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+    0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+    0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+    0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+    0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+    0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+    0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+    0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+    0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+    0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+    0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+    0xF0, 0x80, 0xF0, 0x80, 0x80, // F
+];
+
+// NOTE: This may be replaces with underlying bitvec to save space eventually
 
 /// A boolean array representing the state of the display
 pub(crate) struct Display {
     /// Underlying data representing the display (row major matrix)
-    data: [bool; NUM_ROWS * NUM_COLS],
+    data: [bool; DISPLAY_ROWS * DISPLAY_COLS],
     /// Whether the display needs to be redrawn
     needs_redraw: bool,
 }
@@ -30,7 +61,7 @@ impl Display {
     /// Create an empty display
     fn new() -> Self {
         Display {
-            data: [false; NUM_ROWS * NUM_COLS],
+            data: [false; DISPLAY_ROWS * DISPLAY_COLS],
             needs_redraw: false,
         }
     }
@@ -51,6 +82,16 @@ impl Display {
             .data
             .get(row * ROW_STRIDE + col * COL_STRIDE)
             .context("Tried to index past display bounds!")?));
+    }
+
+    /// XOR the element at the specified row and column
+    fn xor(&mut self, row: usize, col: usize, val: bool) -> Result<()> {
+        let el = self
+            .data
+            .get_mut(row * ROW_STRIDE + col * COL_STRIDE)
+            .context("Tried to index past display bounds!")?;
+        *el ^= val;
+        Ok(())
     }
 
     /// Return an iterator over the elements of the display
@@ -96,7 +137,7 @@ pub(crate) struct Emulator {
 
 impl Emulator {
     /// Create a new Emulator with zeroed fields
-    fn new() -> Self {
+    fn new() -> Result<Self> {
         // Create the sound and delay timers
         let delay_timer = Arc::new(Mutex::new(0u8));
         let sound_timer = Arc::new(Mutex::new(0u8));
@@ -145,11 +186,13 @@ impl Emulator {
             }
         });
 
-        // Create the empty memory and display
+        // Create the empty memory, add the font characters
         let memory: Vec<u8> = Vec::with_capacity(4096);
+
+        // Create the empty display
         let display = Display::new();
 
-        Self {
+        let mut emulator = Self {
             memory,
             display,
             program_counter: 0,
@@ -161,7 +204,9 @@ impl Emulator {
             sound_timer,
             ticker_handle: Some(ticker_handle),
             ticker_channel: sender,
-        }
+        };
+        emulator.load_font().context("Trying to create emulator")?;
+        Ok(emulator)
     }
 
     /// Add a value to the stack
@@ -185,6 +230,17 @@ impl Emulator {
             .context("Invalid stack pointer")?))
     }
 
+    /// Load the font into memory starting at FONT_START_POSITION
+    fn load_font(&mut self) -> Result<()> {
+        for (idx, byte) in (FONT_START_POSITION..(FONT_START_POSITION + FONT.len())).zip(FONT) {
+            *(self
+                .memory
+                .get_mut(idx)
+                .context("Trying to load font into emulator memory")?) = byte
+        }
+        Ok(())
+    }
+
     /// Read a file, loads into memory starting at position 0x200 (512)
     fn load_file<P: AsRef<Path>>(&mut self, path: P) -> Result<()> {
         let contents = std::fs::read(path).context("Failed to read input file")?;
@@ -200,5 +256,71 @@ impl Emulator {
         }
 
         Ok(())
+    }
+
+    /// Draw a sprite to the screen
+    ///
+    /// Starting from the byte in memory at sprite_index, with length/height sprite_length,
+    /// draw the sprite at the row given by y_pos, and the columns given by x_pos.
+    fn draw_sprite(
+        &mut self,
+        sprite_index: usize,
+        sprite_length: usize,
+        x_pos: usize,
+        y_pos: usize,
+    ) -> Result<()> {
+        let mut cur_index = sprite_index;
+        // The x and y coordinates are allowed to wrap
+        let x_pos = x_pos % DISPLAY_COLS;
+        let y_pos = y_pos % DISPLAY_ROWS;
+
+        // Loop through the sprite, xoring with the display bits
+        for row_offset in 0..sprite_length {
+            // If off bottom of screen, stop trying to draw
+            if y_pos + row_offset >= DISPLAY_ROWS {
+                break;
+            };
+            // Get the byte for the current row of the sprite
+            let mut sprite_byte = self
+                .memory
+                .get(cur_index)
+                .context("Trying to get byte in sprite")?
+                .to_owned();
+            for col_offset in 0..SPRITE_WIDTH {
+                // Stop trying to draw if going off screen
+                if x_pos + col_offset >= DISPLAY_COLS {
+                    break;
+                };
+                // XOR the display bit with the value of the sprite at this index
+                // offset (tracked by shifting the sprite byte to the left)
+                self.display.xor(
+                    y_pos + row_offset,
+                    x_pos + col_offset,
+                    (sprite_byte & 0b10000000) == 0b10000000,
+                )?;
+                // Shift the sprite_byte, which will result in the bit of interest being
+                // at the most significant position
+                sprite_byte <<= 1;
+            }
+            // Increment the memory index
+            cur_index += 1;
+        }
+        Ok(())
+    }
+
+    /// Fetch the current instruction (incrementing the program counter appropriately)
+    fn fetch(&mut self) -> Result<(u8, u8)> {
+        let b1 = self
+            .memory
+            .get(self.program_counter)
+            .context("Trying to fetch first byte of instruction")?
+            .to_owned();
+        let b2 = self
+            .memory
+            .get(self.program_counter + 1)
+            .context("Trying to fetch second byte of instruction")?
+            .to_owned();
+        self.program_counter += INSTRUCTION_LENGTH;
+        Ok((b1, b2))
     }
 }
